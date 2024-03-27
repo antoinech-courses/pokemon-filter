0d1e5a95fd79efadc205c2f8248e6198ef3b8561
Antoine CHEUCLE <antoine.cheucle@imt-atlantique.net>
2024-03-27 15:01:27 +0100

    feat: Implement filtering buttons
    Add JS code to filter lines depending on clicked button with jQuery

---
+++ filter.js
@@ -0,0 +1,28 @@
+$(onLoad)
+
+function onLoad() {
+    $('#filter-all').on('click', () => {filterPokemon(predicateAll)});
+    $('#filter-weight').on('click', () => {filterPokemon(predicateWeight)});
+    $('#filter-height').on('click', () => {filterPokemon(predicateHeight)});
+}
+
+function filterPokemon(predicate){
+    $("tbody tr").hide(); // Hide all rows
+    $("tbody tr").filter(function () {console.log($(this));
+        return predicate($(this))}).show(); //Show selected rows
+}
+
+function predicateAll(object){
+    // Show all pokemons
+    return true;
+}
+
+function predicateWeight(object){
+    // Show all pokemons with weight greater than 3
+    return parseFloat(object.find(".pokemon-weight").text()) > 3;
+}
+
+function predicateHeight(object){
+    // Show all pokemons with height less than 1
+    return parseFloat(object.find(".pokemon-height").text()) < 1;
+}
